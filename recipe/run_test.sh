e378098057cd4f65b042fc5bc5f31b02db583324
Marcel Bargull <marcel.bargull@udo.edu>
2020-01-15 17:34:56 +0100

    Revert "test: set _PYTHON_HOST_PLATFORM=macosx-10.13-x86_64"
    This reverts commit b206cfea2766b74d9ff744db5a4288e1b91d7640.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,4 +1,1 @@
-if [ "$(uname)" == Darwin ] ; then
-    export _PYTHON_HOST_PLATFORM=macosx-${MACOSX_DEPLOYMENT_TARGET}-x86_64
-fi
-py.test -k "not test_fortran_compiler and not test_issue352_isolated_environment_support and not test_cmake_args_keyword_osx_default" -v
+py.test -k "not test_fortran_compiler and not test_issue352_isolated_environment_support" -v
