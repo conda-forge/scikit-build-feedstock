78994ccdbcef16703af6584a633186a73c82ec94
Henry Schreiner <HenrySchreinerIII@gmail.com>
2022-04-07 23:47:31 -0400

    Update run_test.bat

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -6,4 +6,4 @@ call rmpath C:\\ProgramData\\Chocolatey\\bin
 call rmpath C:\\Strawberry\\c\\bin
 popd
 
-py.test -k "not test_fortran_compiler and not test_issue352_isolated_environment_support and not test_cmake_install_dir_keyword and not test_generator_selection and not pep518" -v
+py.test -k "not test_fortran_compiler and not test_issue352_isolated_environment_support and not test_cmake_install_dir_keyword and not test_generator_selection and not pep518 and not test_configure_with_cmake_args" -v
