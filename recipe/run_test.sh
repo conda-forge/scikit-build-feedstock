60c8eb0a4d68cac171da5321e425d95bdfc7cb62
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-01-19 23:17:08 -0500

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,1 +1,1 @@
-pytest -m "not isolated and not deprecated" -v
+pytest -m "not isolated and not deprecated and not fortran" --deselect test_isolated_env_trigger_reconfigure --deselect test_generator_cleanup --deselect test_configure_with_cmake_args -v
