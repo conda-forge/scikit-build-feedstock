ec2d0b251c596a3009b08154910318c5aa7df805
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-01-19 23:16:42 -0500

    Update run_test.bat

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -6,4 +6,4 @@ call rmpath C:\\ProgramData\\Chocolatey\\bin
 call rmpath C:\\Strawberry\\c\\bin
 popd
 
-pytest -m "not isolated and not deprecated" -v
+pytest -m "not isolated and not deprecated and not fortran" --deselect test_isolated_env_trigger_reconfigure --deselect test_generator_cleanup --deselect test_configure_with_cmake_args -v
